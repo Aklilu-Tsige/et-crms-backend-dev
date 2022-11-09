7677b4a2ec55dfb7c675cbdb3bde19f782c63c8e
Aklilu Tsige <aklilu.tsige.w@gmail.com>
2022-11-09 09:56:40 +0100

    CRMS-Init

---
+++ src/main/java/org/ethiopiancloud/crms/OrgEthiopiancloudCrmsApplication.java
@@ -0,0 +1,13 @@
+package org.ethiopiancloud.crms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgEthiopiancloudCrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgEthiopiancloudCrmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ethiopiancloud/crms/OrgEthiopiancloudCrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ethiopiancloud.crms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgEthiopiancloudCrmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
