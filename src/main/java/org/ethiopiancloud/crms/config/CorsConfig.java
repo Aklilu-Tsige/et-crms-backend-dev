ad2e19e367b9b184a64330d2c8f982ed9f90af8d
Aklilu Tsige <aklilu.tsige.w@gmail.com>
2022-11-16 08:36:36 +0100

    Configuration CORS for FE

--- src/main/java/org/ethiopiancloud/crms/config/CorsConfig.java
+++ src/main/java/org/ethiopiancloud/crms/config/CorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    private final String urlRequest = "http://localhost:3000";
+    private final String urlRequest = "http://localhost:4200";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
